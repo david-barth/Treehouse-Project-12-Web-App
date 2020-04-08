d9cee08e55e1044e132864c8e08f23f03b96a712
david-barth <41875403+david-barth@users.noreply.github.com>
2020-04-08 22:31:13 +0100

    Add files via upload

--- Backend_Interface/app.js
+++ Backend_Interface/app.js
@@ -1,4 +1,6 @@
 var createError = require('http-errors');
+var debug = require('debug')('main:server');
+var http = require('http');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -6,8 +8,14 @@ var logger = require('morgan');
 var cors = require("cors");
 
 var indexRouter = require('./routes/index');
-
 var app = express();
+
+
+//Set up port and server: 
+var port = normalizePort('9000');
+app.set('port', port);
+var server = http.createServer(app);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,5 +45,65 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//Listen on port: 
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+
+//Functions for port listening: 
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
 
 module.exports = app;
